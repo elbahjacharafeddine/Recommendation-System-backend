0561b0b6f15e5809bfc9cd90033fc99453686628
ElmansouriAMINE <amineelmansouri2001@gmail.com>
2023-04-29 01:41:18 +0100

    mycommit

--- api/user/models.py
+++ api/user/models.py
@@ -8,14 +8,14 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **kwargs):
+    def create_user(self, username, email, password=None, is_superuser=False, **kwargs):
         """Create and return a `User` with an email, username and password."""
         if username is None:
             raise TypeError("Users must have a username.")
         if email is None:
             raise TypeError("Users must have an email.")
 
-        user = self.model(username=username, email=self.normalize_email(email))
+        user = self.model(username=username, email=self.normalize_email(email), is_superuser=is_superuser)
         user.set_password(password)
         user.save(using=self._db)
 
